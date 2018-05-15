b1d633d8d8331490a08e83ab0305d85ce6e639a0
Brandon Nelson <bkonelson@hotmail.com>
2018-05-15 10:29:09 -0600

    Made changes to migration to accept mainApp

--- tinbullet/urls.py
+++ tinbullet/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('youtube/', include('youtube.urls')),
+    path('mainApp/', include('mainApp.urls')),
     path('admin/', admin.site.urls),
 ]
